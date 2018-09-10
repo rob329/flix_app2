abb9b86db175bb8ba1bf48fc0a238e8e40e1cd91
Robert Bolt <robertbolt@Roberts-MacBook-Air.local>
2018-09-10 01:06:57 -0500

    uplaoding code for Flix App

---
+++ Pods/Target Support Files/Pods-flix_app2Tests/Pods-flix_app2Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_flix_app2Tests : NSObject
+@end
+@implementation PodsDummy_Pods_flix_app2Tests
+@end
---
+++ flix_app2/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  flix_app2
+//
+//  Created by Robert Bolt on 9/8/18.
+//  Copyright © 2018 Robert Bolt. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+     @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
