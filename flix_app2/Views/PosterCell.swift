3613b4a72f6cc7d07bc7fa5b71ff062a877196b4
Robert Bolt <robertbolt@Roberts-MacBook-Air.local>
2018-09-17 14:38:35 -0500

    Finishing Assignment

---
+++ flix_app2/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_app2
+//
+//  Created by Robert Bolt on 9/11/18.
+//  Copyright © 2018 Robert Bolt. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
